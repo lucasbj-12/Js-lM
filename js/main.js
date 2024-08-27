c398263ba897b8ce01658f26e18fb7a2973f0e1e
lucas martinez <lucasm.lm314gmail.com>
2024-08-26 22:50:08 -0300

    entrega incompleta

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nombre = prompt ("Ingrese su nombre:");
+let apellido = prompt ("Ingrese su apellido:");
+console.log ("Hola " + nombre + " " + apellido + "!");
+
+let nacimiento = parseInt(prompt("Ingrese su año de nacimiento"));
+let anio_actual = 2024;
+let resultado = anio_actual - nacimiento;
+console.log(resultado);
+
