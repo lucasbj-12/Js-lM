7083bf6523fd5ae2fb361898b4dcf525244b8779
lucas martinez <lucasm.lm314gmail.com>
2024-09-10 18:24:51 -0300

    primera preentrega

--- js/main.js
+++ js/main.js
@@ -7,3 +7,34 @@ let anio_actual = 2024;
 let resultado = anio_actual - nacimiento;
 console.log(resultado);
 
+////////! Algoritmo condicional//////////////
+let peso = parseInt(prompt ("Ingrese su peso actual:"));
+if(peso !==""){
+    alert("Su peso actual es de: " + peso + " Kg.");
+} else{
+    alert("Debe ingresar su peso actual!")
+};
+
+////////! Opearodes Avanzados //////////////
+
+let alumnos = "Lucas";
+
+for (let i = 1; i < 5; i++){
+    if (i === 2){
+        continue;
+    }
+    console.log("Cantidad de dias de entrenamiento: " +i);
+};
+
+////////! Opearodes Avanzados //////////////
+
+function preguntarEjercicios(){
+let ejercicios = prompt ("Que parte de su cuerpo le gustaria entrenar?")
+console.log ("Usted entrenara: " + ejercicios)
+};
+
+preguntarEjercicios();
+preguntarEjercicios();
+preguntarEjercicios();
+
+////////! Funciones //////////////
